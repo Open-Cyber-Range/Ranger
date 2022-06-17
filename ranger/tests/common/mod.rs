061efc902136efa6b3d430138edc05b1732f9ce9
Madis-Mikk Remmet <madismikk.remmet@cr14.ee>
2022-06-17 14:15:37 +0300

    add tests

--- ranger/tests/common/mod.rs
+++ ranger/tests/common/mod.rs
@@ -1,12 +1,35 @@
+use actix::Actor;
+use actix_web::web::Data;
 use anyhow::Result;
+use lazy_static::lazy_static;
 use rand::Rng;
+use ranger::{
+    configuration::Configuration,
+    database::{AddScenario, Database},
+    deployer::DeploymentManager,
+    node::NodeClient,
+    AppState,
+};
 use ranger_grpc::{
     node_service_server::{NodeService, NodeServiceServer},
     Identifier, NodeDeployment, NodeIdentifier, NodeType,
 };
+
 use std::time::Duration;
 use tokio::runtime::Runtime;
 use tonic::{transport::Server, Request, Response, Status};
+
+lazy_static! {
+    pub static ref CONFIGURATION: Configuration = Configuration {
+        node_deployer_addresses: vec![
+            "http://localhost:9999".to_string(),
+            "http://localhost:9998".to_string(),
+            "http://localhost:9997".to_string(),
+        ],
+        host: "localhost".to_string(),
+        port: 3000,
+    };
+}
 
 pub struct MockNodeService {
     builder: MockNodeBuilder,
@@ -23,15 +46,12 @@ impl NodeService for MockNodeService {
     async fn create(&self, _: Request<NodeDeployment>) -> Result<Response<NodeIdentifier>, Status> {
         if self.builder.successful_create {
             Status::ok("Node created successfully");
-            return Ok(
-                Response::new(
-                    NodeIdentifier { 
-                        identifier: Some(Identifier{
-                            value: String::from("Some UUID")
-                        }),
-                        node_type: NodeType::Vm.into(),
-                    }
-                ));
+            return Ok(Response::new(NodeIdentifier {
+                identifier: Some(Identifier {
+                    value: String::from("Some UUID"),
+                }),
+                node_type: NodeType::Vm.into(),
+            }));
         }
 
         return Err(Status::internal("Failed to create node"));
@@ -111,5 +131,48 @@ impl MockNodeBuilder {
         std::thread::sleep(Duration::from_millis(self.timeout_millis));
 
         Ok(server_address_string)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+    use ranger::routes::exercise::add_exercise;
+    use sdl_parser::test::{TEST_SCHEMA, TEST_SCHEMA_STRING};
+    
+
+
+    pub async fn create_test_app_state() -> Data<AppState> {
+        let socket_address = crate::common::create_mock_node_server().run().unwrap();
+        let node_client_address = NodeClient::new(format!("http://{}", socket_address))
+            .await
+            .unwrap()
+            .start();
+        let database_address = Database::new().start();
+        database_address
+            .send(AddScenario(TEST_SCHEMA.scenario.clone()))
+            .await
+            .unwrap();
+        let deployer_address = DeploymentManager::new(node_client_address).start();
+        let app_state = AppState {
+            database_address,
+            deployer_address,
+        };
+        Data::new(app_state)
+    }
+
+    #[actix_web::test]
+    pub async fn node_deployed_successfully() -> Result<()> {
+        let app_state = create_test_app_state().await;
+        let app = test::init_service(App::new().app_data(app_state).service(add_exercise)).await;
+        let payload = Vec::try_from(TEST_SCHEMA_STRING)?;
+        let request = test::TestRequest::post()
+            .uri("/exercise")
+            .set_payload(payload)
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert_eq!(response.status(), 200);
+        Ok(())
     }
 }
