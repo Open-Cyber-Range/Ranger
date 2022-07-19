993d6a02f333059eb7ea23b3ea139120c23bfd3d
Rait Rotšan <rait.rotsan@cr14.ee>
2022-07-19 14:32:33 +0300

    Switch qualifiers to use imports in tests

--- ranger/tests/capability.rs
+++ ranger/tests/capability.rs
@@ -2,7 +2,7 @@ mod common;
 
 #[cfg(test)]
 mod tests {
-
+    use crate::common::create_mock_capability_server;
     use actix::Actor;
     use actix_rt::System;
     use anyhow::{Error, Result};
@@ -11,7 +11,7 @@ mod tests {
 
     #[test]
     fn successful_capability_response() -> Result<()> {
-        let socket_address = crate::common::create_mock_capability_server().run()?;
+        let socket_address = create_mock_capability_server().run()?;
         let system = System::new();
         let capabilities = system.block_on(async {
             let node_deployer_client = NodeClient::new(format!("http://{}", socket_address))
--- ranger/tests/node.rs
+++ ranger/tests/node.rs
@@ -2,17 +2,19 @@ mod common;
 
 #[cfg(test)]
 mod tests {
+    use crate::common::create_mock_vmware_server;
     use actix::Actor;
     use actix_rt::System;
     use anyhow::{Error, Result};
     use ranger::node::{CreateNode, DeleteNode, NodeClient};
     use ranger_grpc::{
-        Configuration, DeploymentParameters, Identifier, Node, NodeIdentifier, NodeType,
+        Configuration, DeploymentParameters, Identifier, Node, NodeDeployment, NodeIdentifier,
+        NodeType,
     };
 
     #[test]
     fn node_created_successfully() -> Result<()> {
-        let socket_address = crate::common::create_mock_vmware_server().run_node_server()?;
+        let socket_address = create_mock_vmware_server().run_node_server()?;
         let system = System::new();
         let node_id = system.block_on(async {
             let node_deployer_client =
@@ -20,7 +22,7 @@ mod tests {
                     .await?
                     .start();
             let node_id = node_deployer_client
-                .send(CreateNode(ranger_grpc::NodeDeployment {
+                .send(CreateNode(NodeDeployment {
                     parameters: Some(DeploymentParameters {
                         name: "some-name".to_string(),
                         exercise_name: "some-exercise".to_string(),
@@ -47,7 +49,7 @@ mod tests {
 
     #[test]
     fn node_creation_failed() -> Result<()> {
-        let socket_address = crate::common::create_mock_vmware_server()
+        let socket_address = create_mock_vmware_server()
             .successful_create(false)
             .run_node_server()?;
         let system = System::new();
@@ -57,7 +59,7 @@ mod tests {
                     .await?
                     .start();
             node_deployer_client
-                .send(CreateNode(ranger_grpc::NodeDeployment {
+                .send(CreateNode(NodeDeployment {
                     parameters: Some(DeploymentParameters {
                         name: "some-name".to_string(),
                         exercise_name: "some-exercise".to_string(),
@@ -84,7 +86,7 @@ mod tests {
 
     #[test]
     fn node_deleted_successfully() -> Result<()> {
-        let socket_address = crate::common::create_mock_vmware_server().run_node_server()?;
+        let socket_address = create_mock_vmware_server().run_node_server()?;
         let system = System::new();
         system.block_on(async {
             let node_deployer_client =
@@ -92,13 +94,12 @@ mod tests {
                     .await?
                     .start();
             node_deployer_client
-                .send(
-                    DeleteNode(
-                        ranger_grpc::NodeIdentifier{
-                            identifier: Some(Identifier{
-                                value: "some-identifier".to_string()}),
-                            node_type: NodeType::Vm.into(),
-                        }))
+                .send(DeleteNode(NodeIdentifier {
+                    identifier: Some(Identifier {
+                        value: "some-identifier".to_string(),
+                    }),
+                    node_type: NodeType::Vm.into(),
+                }))
                 .await??;
             Ok::<(), Error>(())
         })?;
@@ -108,7 +109,7 @@ mod tests {
 
     #[test]
     fn node_deletion_failed() -> Result<()> {
-        let socket_address = crate::common::create_mock_vmware_server()
+        let socket_address = create_mock_vmware_server()
             .successful_delete(false)
             .run_node_server()?;
         let system = System::new();
@@ -118,13 +119,12 @@ mod tests {
                     .await?
                     .start();
             node_deployer_client
-                .send(
-                    DeleteNode(
-                        ranger_grpc::NodeIdentifier{
-                            identifier: Some(Identifier{
-                                value: "some-identifier".to_string()}),
-                            node_type: NodeType::Vm.into(),
-                        }))
+                .send(DeleteNode(NodeIdentifier {
+                    identifier: Some(Identifier {
+                        value: "some-identifier".to_string(),
+                    }),
+                    node_type: NodeType::Vm.into(),
+                }))
                 .await??;
             Ok::<(), Error>(())
         });
