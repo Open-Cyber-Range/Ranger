a122d1daae1ac549e77459a56f13befdcb4bf5f8
Kristi Reispass <kristi.reispass@cr14.ee>
2022-09-26 10:26:01 +0300

    added basic app and button

---
+++ ranger-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Button } from "@blueprintjs/core";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button text="Click me" />
+      </header>
+    </div >
+  );
+}
+
+export default App;
