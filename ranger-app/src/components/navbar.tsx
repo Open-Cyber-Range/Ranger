6f6f1802a98143747d82aecdcf5c44f4181ed70f
Kristi Reispass <kristi.reispass@cr14.ee>
2022-10-06 12:06:49 +0000

    updated navbar style

--- ranger-app/src/components/navbar.tsx
+++ ranger-app/src/components/navbar.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Alignment, Navbar as Nav } from '@blueprintjs/core';
 
 const Navbar = () => {
     return (
-        <nav style={{ margin: 10 }}>
-            <Link to="/" style={{ padding: 5 }}>
-                Home
-            </Link>
-            <Link to="/exercise" style={{ padding: 5 }}>
-                Exercise
-            </Link>
-        </nav>
+        <Nav fixedToTop={true}>
+            <Nav.Group align={Alignment.LEFT}>
+                <Nav.Heading>Ranger</Nav.Heading>
+                <Nav.Divider />
+                <Link role="button" className="bp4-button bp4-minimal bp4-icon-home" to="/">
+                    Home
+                </Link>
+                <Link role="button" className="bp4-button bp4-minimal bp4-icon-document" to="/exercise">
+                    Exercise
+                </Link>
+            </Nav.Group>
+        </Nav >
     );
 }
 
