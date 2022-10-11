e69e4bd623d696b9c93d915005b9b4adecaa8b86
Kristi Reispass <kristi.reispass@cr14.ee>
2022-10-11 11:52:07 +0000

    form works again, controller overwrite

--- ranger-app/src/pages/Exercise.tsx
+++ ranger-app/src/pages/Exercise.tsx
@@ -1,7 +1,7 @@
 import type {SubmitHandler} from 'react-hook-form';
-import {useForm} from 'react-hook-form';
+import {useForm, Controller} from 'react-hook-form';
 import axios from 'axios';
-import {Button, FormGroup, InputGroup, Intent} from '@blueprintjs/core';
+import {Button, FormGroup, InputGroup, Intent, Label, TextArea} from '@blueprintjs/core';
 import {AppToaster} from '../components/Toaster';
 
 type Exercise = {
@@ -10,15 +10,20 @@ type Exercise = {
 };
 
 const ExerciseForm = () => {
-  const {register, handleSubmit, formState: {errors}} = useForm<Exercise>();
+  const {register, handleSubmit, control, formState: {errors}} = useForm<Exercise>({
+    defaultValues: {
+      name: '',
+      scenario: '',
+    },
+  });
 
   const onSubmit: SubmitHandler<Exercise> = async exercise => {
     try {
-      const response = axios.post<Exercise>('api/v1/exercise');
+      const response = await axios.post<Exercise>('api/v1/exercise', exercise);
       AppToaster.show({
         icon: 'tick',
         intent: Intent.SUCCESS,
-        message: 'Exercise added!',
+        message: `Exercise "${exercise.name}" added`,
       });
     } catch {
       AppToaster.show({
@@ -33,27 +38,53 @@ const ExerciseForm = () => {
     <div >
       <h3>Add new exercise</h3>
       <form className='ExerciseForm' onSubmit={handleSubmit(onSubmit)} >
-        <FormGroup
-          label='Exercise name'
-          labelFor='exercise-name'
-          inline>
-          <InputGroup
-            id='exercise-name'
-            placeholder='exercise-1'
-          />
-        </FormGroup>
-        <FormGroup
-          label='Scenario yaml'
-          labelFor='scenario'
-          inline>
-          <InputGroup
-            id='scenario'
-            placeholder='scenario: ...'
-          />
-        </FormGroup>
-        <Button type='submit' > Submit </Button>
+        <Controller
+          control={control}
+          name='name'
+          rules={{required: 'Exercise must have a name'}}
+          render={({field: {onChange, onBlur, ref, value}, fieldState: {error}}) => {
+            const intent = error ? Intent.DANGER : Intent.NONE;
+            return (
+              <FormGroup labelFor='execise-name' labelInfo='(required)' helperText={error?.message} intent={intent} label='Exercise name'>
+                <InputGroup
+                  intent={intent}
+                  value={value}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  inputRef={ref}
+                  id='execise-name'
+                  large
+                />
+              </FormGroup>
+            );
+          }}
+        />
+        <Controller
+          control={control}
+          name='scenario'
+          rules={{required: 'Exercise must have a scenario'}}
+          render={({field: {onChange, onBlur, ref, value}, fieldState: {error}}) => {
+            const intent = error ? Intent.DANGER : Intent.NONE;
+            return (
+              <FormGroup labelFor='scenario' labelInfo='(required)' helperText={error?.message} intent={intent} label='Scenario SDL'>
+                <TextArea
+                  intent={intent}
+                  value={value}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  inputRef={ref}
+                  id='scenario'
+                  small
+                  fill
+                  growVertically
+                  rows={20}
+                />
+              </FormGroup>
+            );
+          }}
+        />
+        <Button type='submit' intent='primary'> Submit </Button>
       </form>
-
     </div>
   );
 };
