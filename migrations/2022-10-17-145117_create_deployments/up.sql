395cd6fecc36d09b60a333dae7a4a8c16846a24d
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-10-22 14:52:11 +0000

    remove anything scenario related

--- migrations/2022-10-17-145117_create_deployments/up.sql
+++ migrations/2022-10-17-145117_create_deployments/up.sql
@@ -2,12 +2,10 @@ CREATE TABLE deployments (
     id BINARY(16) NOT NULL,
     name TEXT NOT NULL,
     deployment_group TEXT,
-    scenario_id BINARY(16) NOT NULL,
-    exercise_id BINARY(16) NOT NULL,
+    sdl_schema LONGTEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (scenario_id) REFERENCES scenarios(id),
     FOREIGN KEY (exercise_id) REFERENCES exercises(id)
 );
