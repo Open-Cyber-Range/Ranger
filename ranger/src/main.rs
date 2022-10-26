a30e22fde2ddd15818abb266a81935e43092d17f
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-10-26 14:02:59 +0000

    add deployment elements API

--- ranger/src/main.rs
+++ ranger/src/main.rs
@@ -2,7 +2,9 @@ use actix_web::web::scope;
 use actix_web::{web::Data, App, HttpServer};
 use anyhow::Error;
 use ranger::app_setup;
-use ranger::routes::exercise::{delete_exercise_deployment, get_exercises, update_exercise};
+use ranger::routes::exercise::{
+    delete_exercise_deployment, get_exercise_deployment_elements, get_exercises, update_exercise,
+};
 use ranger::routes::{
     basic::{status, version},
     exercise::{add_exercise, add_exercise_deployment, delete_exercise},
@@ -22,6 +24,7 @@ async fn main() -> Result<(), Error> {
             .service(version)
             .service(
                 scope("/api/v1")
+                    .service(get_exercise_deployment_elements)
                     .service(add_exercise_deployment)
                     .service(delete_exercise_deployment)
                     .service(get_exercises)
