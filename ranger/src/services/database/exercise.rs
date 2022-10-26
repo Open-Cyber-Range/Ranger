0baacc8ca6e1dbd52026c5f93899bb0e6ada6fee
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-10-26 07:40:38 +0000

    handle diesel in async manner

--- ranger/src/services/database/exercise.rs
+++ ranger/src/services/database/exercise.rs
@@ -2,8 +2,9 @@ use super::Database;
 use crate::constants::RECORD_NOT_FOUND;
 use crate::models::helpers::uuid::Uuid;
 use crate::models::{Exercise, NewExercise};
-use actix::{Handler, Message};
-use anyhow::{anyhow, Result};
+use actix::{Handler, Message, ResponseActFuture, WrapFuture};
+use actix_web::web::block;
+use anyhow::{anyhow, Ok, Result};
 use diesel::RunQueryDsl;
 
 #[derive(Message)]
@@ -11,16 +12,27 @@ use diesel::RunQueryDsl;
 pub struct CreateExercise(pub NewExercise);
 
 impl Handler<CreateExercise> for Database {
-    type Result = Result<Exercise>;
+    type Result = ResponseActFuture<Self, Result<Exercise>>;
 
     fn handle(&mut self, msg: CreateExercise, _ctx: &mut Self::Context) -> Self::Result {
         let new_exercise = msg.0;
-        let mut connection = self.get_connection()?;
+        let connection_result = self.get_connection();
 
-        new_exercise.create_insert().execute(&mut connection)?;
-        let exercise = Exercise::by_id(new_exercise.id).first(&mut connection)?;
+        Box::pin(
+            async move {
+                let mut connection = connection_result?;
+                let exercise = block(move || {
+                    new_exercise.create_insert().execute(&mut connection)?;
+                    let exercise = Exercise::by_id(new_exercise.id).first(&mut connection)?;
 
-        Ok(exercise)
+                    Ok(exercise)
+                })
+                .await??;
+
+                Ok(exercise)
+            }
+            .into_actor(self),
+        )
     }
 }
 
@@ -29,15 +41,26 @@ impl Handler<CreateExercise> for Database {
 pub struct GetExercise(pub Uuid);
 
 impl Handler<GetExercise> for Database {
-    type Result = Result<Exercise>;
+    type Result = ResponseActFuture<Self, Result<Exercise>>;
 
     fn handle(&mut self, msg: GetExercise, _ctx: &mut Self::Context) -> Self::Result {
         let uuid = msg.0;
-        let mut connection = self.get_connection()?;
+        let connection_result = self.get_connection();
 
-        let exercise = Exercise::by_id(uuid).first(&mut connection)?;
+        Box::pin(
+            async move {
+                let mut connection = connection_result?;
+                let exercise = block(move || {
+                    let exercise = Exercise::by_id(uuid).first(&mut connection)?;
 
-        Ok(exercise)
+                    Ok(exercise)
+                })
+                .await??;
+
+                Ok(exercise)
+            }
+            .into_actor(self),
+        )
     }
 }
 
@@ -46,13 +69,25 @@ impl Handler<GetExercise> for Database {
 pub struct GetExercises;
 
 impl Handler<GetExercises> for Database {
-    type Result = Result<Vec<Exercise>>;
+    type Result = ResponseActFuture<Self, Result<Vec<Exercise>>>;
 
     fn handle(&mut self, _: GetExercises, _ctx: &mut Self::Context) -> Self::Result {
-        let mut connection = self.get_connection()?;
-        let exercises = Exercise::all().load(&mut connection)?;
+        let connection_result = self.get_connection();
 
-        Ok(exercises)
+        Box::pin(
+            async move {
+                let mut connection = connection_result?;
+                let exercise = block(move || {
+                    let exercises = Exercise::all().load(&mut connection)?;
+
+                    Ok(exercises)
+                })
+                .await??;
+
+                Ok(exercise)
+            }
+            .into_actor(self),
+        )
     }
 }
 
@@ -61,22 +96,33 @@ impl Handler<GetExercises> for Database {
 pub struct UpdateExercise(pub Uuid, pub crate::models::UpdateExercise);
 
 impl Handler<UpdateExercise> for Database {
-    type Result = Result<Exercise>;
+    type Result = ResponseActFuture<Self, Result<Exercise>>;
 
     fn handle(&mut self, msg: UpdateExercise, _ctx: &mut Self::Context) -> Self::Result {
         let uuid = msg.0;
         let update_exercise = msg.1;
+        let connection_result = self.get_connection();
 
-        let mut connection = self.get_connection()?;
-        let updated_rows = update_exercise
-            .create_update(uuid)
-            .execute(&mut connection)?;
-        if updated_rows != 1 {
-            return Err(anyhow!(RECORD_NOT_FOUND));
-        }
-        let exercise = Exercise::by_id(uuid).first(&mut connection)?;
+        Box::pin(
+            async move {
+                let mut connection = connection_result?;
+                let exercise = block(move || {
+                    let updated_rows = update_exercise
+                        .create_update(uuid)
+                        .execute(&mut connection)?;
+                    if updated_rows != 1 {
+                        return Err(anyhow!(RECORD_NOT_FOUND));
+                    }
+                    let exercise = Exercise::by_id(uuid).first(&mut connection)?;
 
-        Ok(exercise)
+                    Ok(exercise)
+                })
+                .await??;
+
+                Ok(exercise)
+            }
+            .into_actor(self),
+        )
     }
 }
 
@@ -85,15 +131,26 @@ impl Handler<UpdateExercise> for Database {
 pub struct DeleteExercise(pub Uuid);
 
 impl Handler<DeleteExercise> for Database {
-    type Result = Result<Uuid>;
+    type Result = ResponseActFuture<Self, Result<Uuid>>;
 
     fn handle(&mut self, msg: DeleteExercise, _ctx: &mut Self::Context) -> Self::Result {
         let id = msg.0;
-        let mut connection = self.get_connection()?;
+        let connection_result = self.get_connection();
 
-        let exercise = Exercise::by_id(id).first(&mut connection)?;
-        exercise.soft_delete().execute(&mut connection)?;
+        Box::pin(
+            async move {
+                let mut connection = connection_result?;
+                let id = block(move || {
+                    let exercise = Exercise::by_id(id).first(&mut connection)?;
+                    exercise.soft_delete().execute(&mut connection)?;
 
-        Ok(id)
+                    Ok(id)
+                })
+                .await??;
+
+                Ok(id)
+            }
+            .into_actor(self),
+        )
     }
 }
