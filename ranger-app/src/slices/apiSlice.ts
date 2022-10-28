8b1808a1aa98aec9805fcf6da47a8d865b1b5581
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-10-28 14:12:12 +0000

    add proper caching for elements in the apislice

--- ranger-app/src/slices/apiSlice.ts
+++ ranger-app/src/slices/apiSlice.ts
@@ -6,17 +6,25 @@ import type {Exercise, NewExercise, UpdateExercise} from 'src/models/Exercise';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
+  tagTypes: ['Deployment', 'Exercise'],
   endpoints: builder => ({
     getExercises: builder.query<Exercise[], void>({
       query: () => '/exercise',
+      providesTags: (result = []) =>
+        [
+          ...result.map(({id}) => ({type: 'Exercise' as const, id})),
+          {type: 'Exercise', id: 'LIST'},
+        ],
     }),
     getExercise: builder.query<Exercise, string>({
       query: exerciseId => `/exercise/${exerciseId}`,
+      providesTags: (result, error, id) => [{type: 'Exercise', id}],
     }),
     addExercise: builder.mutation<Exercise, NewExercise>({
       query: newExercise => ({
         url: '/exercise', method: 'POST', body: newExercise,
       }),
+      invalidatesTags: [{type: 'Exercise', id: 'LIST'}],
     }),
     updateExercise: builder.mutation<Exercise, {
       exerciseUpdate: UpdateExercise; exerciseId: string;
@@ -24,9 +32,16 @@ export const apiSlice = createApi({
       query: ({exerciseUpdate, exerciseId}) => ({
         url: `/exercise/${exerciseId}`, method: 'PUT', body: exerciseUpdate,
       }),
+      invalidatesTags: (result, error, {exerciseId}) =>
+        [{type: 'Exercise', id: exerciseId}],
     }),
     getDeployments: builder.query<Deployment[], string>({
       query: exerciseId => `/exercise/${exerciseId}/deployment`,
+      providesTags: (result = []) =>
+        [
+          ...result.map(({id}) => ({type: 'Deployment' as const, id})),
+          {type: 'Deployment', id: 'LIST'},
+        ],
     }),
     addDeployment: builder
       .mutation<Deployment,
@@ -38,6 +53,7 @@ export const apiSlice = createApi({
         method: 'POST',
         body: newDeployment,
       }),
+      invalidatesTags: [{type: 'Deployment', id: 'LIST'}],
     }),
   }),
 });
