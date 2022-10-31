d5a67b6d3a6bd4a3a538e65c7333d0365f2abdae
Kristi Reispass <kristi.reispass@cr14.ee>
2022-10-31 12:01:51 +0200

    added suspense loading text

--- ranger-app/src/App.tsx
+++ ranger-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ExerciseDetail from 'src/pages/ExerciseDetail';
@@ -7,14 +7,16 @@ import MainNavbar from 'src/components/MainNavBar';
 import Home from 'src/pages/Home';
 
 const App = () => (
-  <Router>
-    <MainNavbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/exercises' element={<Exercises/>}/>
-      <Route path='/exercises/:exerciseId' element={<ExerciseDetail/>}/>
-    </Routes>
-  </Router>
+  <Suspense fallback="Loading...">
+    <Router>
+      <MainNavbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercises' element={<Exercises/>}/>
+        <Route path='/exercises/:exerciseId' element={<ExerciseDetail/>}/>
+      </Routes>
+    </Router>
+  </Suspense>
 );
 
 export default App;
