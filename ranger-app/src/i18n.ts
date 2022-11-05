d0a0d1f095d941ae680e12df140a2b1f904e7acd
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-11-05 13:12:55 +0000

    remove backend fetching for translations

--- ranger-app/src/i18n.ts
+++ ranger-app/src/i18n.ts
@@ -1,17 +1,21 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translation from 'src/locales/en/translation';
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18n // eslint-disable-line import/no-named-as-default-member
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next).init({
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation,
+      },
     },
   });
 
---
+++ ranger-app/src/locales/en/translation.ts
@@ -0,0 +1,18 @@
+const language = {
+  homepage: 'Welcome to Ranger!',
+  exercises: {
+    title: 'Exercises',
+    add: 'Add exercise',
+    addingSuccess: 'Exercise {{exerciseName}} added',
+    addingFail: 'Failed to add the exercise',
+  },
+  deployments: {
+    title: 'Deployments',
+    add: 'Add Deployment',
+    addingSuccess: 'Deployment {{newDeploymentName}} added',
+    addingFail: 'Failed to add the deployment',
+    sdlMissing: 'Exercise must have an sdl-schema',
+  },
+};
+
+export default language;
