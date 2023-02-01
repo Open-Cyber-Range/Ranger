887d350324ca80be49ed8581ac4cab31844b9074
Kristi Reispass <kristi.reispass@cr14.ee>
2023-02-01 11:05:03 +0200

    add constants for deleted_at default value check

--- ranger/src/constants.rs
+++ ranger/src/constants.rs
@@ -10,3 +10,6 @@ pub const MAX_EXERCISE_NAME_LENGTH: usize = 20;
 pub const RECORD_NOT_FOUND: &str = "Record not found";
 pub const DUPLICATE_ENTRY: &str = "Duplicate entry";
 pub const FOREIGN_KEY_CONSTRAINT_FAILS: &str = "a foreign key constraint fails";
+
+pub const DELETED_AT_DEFAULT_VALUE: &str = "1970-01-01 00:00:01";
+pub const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
