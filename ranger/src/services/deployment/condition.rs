2488a8c9b24e6f808c8af833aefb17411e57e774
Rait Rotšan <rait.rotsan@cr14.ee>
2023-02-07 07:59:58 +0000

    fix multiple condition streams on one vm

--- ranger/src/services/deployment/condition.rs
+++ ranger/src/services/deployment/condition.rs
@@ -8,6 +8,7 @@ use crate::services::deployer::{Deploy, DeployerDistribution};
 use actix::Addr;
 use anyhow::{anyhow, Ok, Result};
 use async_trait::async_trait;
+use futures::future::try_join_all;
 use log::info;
 use ranger_grpc::capabilities::DeployerTypes;
 use ranger_grpc::{Account as GrpcAccount, Condition as GrpcCondition, Source as GrpcSource};
@@ -43,134 +44,142 @@ impl DeployableConditions
             template_id,
         ) = self;
 
-        let node_conditions = node
-            .conditions
-            .clone()
-            .ok_or_else(|| anyhow!("Node conditions not found"))?;
-
         let scenario_conditions = scenario
-            .conditions
             .clone()
+            .conditions
             .ok_or_else(|| anyhow!("Scenario conditions not found"))?;
 
-        let roles = node
-            .clone()
-            .roles
-            .ok_or_else(|| anyhow!("Node Roles not found"))?;
+        try_join_all(
+            scenario_conditions
+                .iter()
+                .map(|(condition_name, condition)| async move {
+                    let node_conditions = node
+                        .clone()
+                        .conditions
+                        .ok_or_else(|| anyhow!("Node conditions not found"))?;
 
-        let template_id = template_id
-            .to_owned()
-            .ok_or_else(|| anyhow!("Template id not found"))?;
+                    let roles = node
+                        .clone()
+                        .roles
+                        .ok_or_else(|| anyhow!("Node Roles not found"))?;
 
-        let virtual_machine_id = deployment_element
-            .handler_reference
-            .clone()
-            .ok_or_else(|| anyhow!("Deployment element handler reference not found"))?;
+                    let template_id = template_id
+                        .to_owned()
+                        .ok_or_else(|| anyhow!("Template id not found"))?;
 
-        for (condition_name, condition) in scenario_conditions.iter() {
-            for (node_condition_name, role_name) in node_conditions.iter() {
-                if condition_name.eq_ignore_ascii_case(node_condition_name) {
-                    info!(
-                        "Deploying condition '{condition_name}' for VM {node_name}",
-                        node_name = deployment_element.scenario_reference
-                    );
+                    let virtual_machine_id = deployment_element
+                        .clone()
+                        .handler_reference
+                        .ok_or_else(|| anyhow!("Deployment element handler reference not found"))?;
+                    // for (condition_name, condition) in scenario_conditions.iter() {
+                    for (node_condition_name, role_name) in node_conditions.iter() {
+                        if condition_name.eq_ignore_ascii_case(node_condition_name) {
+                            info!(
+                                "Deploying condition '{condition_name}' for VM {node_name}",
+                                node_name = deployment_element.scenario_reference
+                            );
 
-                    let (_, username) = roles
-                        .get_key_value(&role_name.clone())
-                        .ok_or_else(|| anyhow!("Username in roles list not found"))?;
+                            let (_, username) = roles
+                                .get_key_value(&role_name.clone())
+                                .ok_or_else(|| anyhow!("Username in roles list not found"))?;
 
-                    let source: Option<GrpcSource> = match condition.clone().source {
-                        Some(condition_source) => Some(GrpcSource {
-                            name: condition_source.name,
-                            version: condition_source.version,
-                        }),
-                        None => None,
-                    };
+                            let source: Option<GrpcSource> = match condition.clone().source {
+                                Some(condition_source) => Some(GrpcSource {
+                                    name: condition_source.name,
+                                    version: condition_source.version,
+                                }),
+                                None => None,
+                            };
 
-                    let template_account = database_address
-                        .send(GetAccount(
-                            template_id.as_str().try_into()?,
-                            username.to_owned(),
-                        ))
-                        .await??;
-
-                    let mut condition_deployment_element = database_address
-                        .send(CreateDeploymentElement(
-                            *exercise_id,
-                            DeploymentElement::new_ongoing(
-                                deployment_element.deployment_id,
-                                Box::new(condition_name.to_owned()),
-                                DeployerTypes::Condition,
-                            ),
-                        ))
-                        .await??;
-
-                    let condition_deployment = Box::new(GrpcCondition {
-                        name: node_condition_name.to_owned(),
-                        source,
-                        command: condition.clone().command.unwrap_or_default(),
-                        interval: condition.interval.unwrap_or_default() as i32,
-                        virtual_machine_id: virtual_machine_id.clone(),
-                        account: Some(GrpcAccount {
-                            username: template_account.username,
-                            password: template_account.password.unwrap_or_default(),
-                            private_key: template_account.private_key.unwrap_or_default(),
-                        }),
-                    });
-
-                    match distributor_address
-                        .send(Deploy(
-                            DeployerTypes::Condition,
-                            condition_deployment,
-                            deployers.to_owned(),
-                        ))
-                        .await?
-                    {
-                        anyhow::Result::Ok(result) => {
-                            let (condition_identifier, condition_stream) =
-                                ConditionResponse::try_from(result)?;
-
-                            condition_deployment_element.status = ElementStatus::Success;
-                            condition_deployment_element.handler_reference =
-                                Some(condition_identifier.value.clone());
-
-                            database_address
-                                .send(UpdateDeploymentElement(
-                                    *exercise_id,
-                                    condition_deployment_element.clone(),
+                            let template_account = database_address
+                                .send(GetAccount(
+                                    template_id.as_str().try_into()?,
+                                    username.to_owned(),
                                 ))
                                 .await??;
 
-                            info!(
+                            let mut condition_deployment_element = database_address
+                                .send(CreateDeploymentElement(
+                                    *exercise_id,
+                                    DeploymentElement::new_ongoing(
+                                        deployment_element.deployment_id,
+                                        Box::new(condition_name.to_owned()),
+                                        DeployerTypes::Condition,
+                                    ),
+                                ))
+                                .await??;
+
+                            let condition_deployment = Box::new(GrpcCondition {
+                                name: node_condition_name.to_owned(),
+                                source,
+                                command: condition.clone().command.unwrap_or_default(),
+                                interval: condition.interval.unwrap_or_default() as i32,
+                                virtual_machine_id: virtual_machine_id.clone(),
+                                account: Some(GrpcAccount {
+                                    username: template_account.username,
+                                    password: template_account.password.unwrap_or_default(),
+                                    private_key: template_account.private_key.unwrap_or_default(),
+                                }),
+                            });
+
+                            match distributor_address
+                                .send(Deploy(
+                                    DeployerTypes::Condition,
+                                    condition_deployment,
+                                    deployers.to_owned(),
+                                ))
+                                .await?
+                            {
+                                anyhow::Result::Ok(result) => {
+                                    let (condition_identifier, condition_stream) =
+                                        ConditionResponse::try_from(result)?;
+
+                                    condition_deployment_element.status = ElementStatus::Success;
+                                    condition_deployment_element.handler_reference =
+                                        Some(condition_identifier.value.clone());
+
+                                    database_address
+                                        .send(UpdateDeploymentElement(
+                                            *exercise_id,
+                                            condition_deployment_element.clone(),
+                                        ))
+                                        .await??;
+
+                                    info!(
                                 "Finished deploying {condition_name} on {}, starting stream",
                                 deployment_element.scenario_reference
                             );
 
-                            distributor_address
-                                .clone()
-                                .send(ConditionStream(
-                                    condition_deployment_element,
-                                    database_address.clone(),
-                                    condition_stream,
-                                ))
-                                .await??;
-                        }
+                                    distributor_address
+                                        .clone()
+                                        .send(ConditionStream(
+                                            condition_deployment_element,
+                                            database_address.clone(),
+                                            condition_stream,
+                                        ))
+                                        .await??;
+                                    return Ok(());
+                                }
 
-                        Err(error) => {
-                            condition_deployment_element.status = ElementStatus::Failed;
+                                Err(error) => {
+                                    condition_deployment_element.status = ElementStatus::Failed;
 
-                            database_address
-                                .send(UpdateDeploymentElement(
-                                    *exercise_id,
-                                    condition_deployment_element,
-                                ))
-                                .await??;
-                            return Err(error);
+                                    database_address
+                                        .send(UpdateDeploymentElement(
+                                            *exercise_id,
+                                            condition_deployment_element,
+                                        ))
+                                        .await??;
+                                    return Err(error);
+                                }
+                            }
                         }
                     }
-                }
-            }
-        }
+                    Ok(())
+                })
+                .collect::<Vec<_>>(),
+        )
+        .await?;
         Ok(())
     }
 }
