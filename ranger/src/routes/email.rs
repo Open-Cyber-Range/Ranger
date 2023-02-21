09ac6ec6dde23ef3a42bc9686a0d719b6d8cf814
Kristi Reispass <kristi.reispass@cr14.ee>
2023-02-21 11:14:25 +0000

    create email model and endpoint

---
+++ ranger/src/routes/email.rs
@@ -0,0 +1,25 @@
+use crate::{errors::RangerError, models::Email, services::mailer::Mailer, AppState};
+use actix_web::{
+    post,
+    web::{Data, Json},
+};
+use anyhow::Result;
+use log::{error, info};
+
+#[post("exercise/{exercise_uuid}/email")]
+pub async fn send_email(
+    app_state: Data<AppState>,
+    email: Json<Email>,
+) -> Result<Json<Email>, RangerError> {
+    let email = email.into_inner();
+    let mailer_configuration = app_state.configuration.mailer_configuration.clone();
+    if let Some(mailer_configuration) = mailer_configuration {
+        let message = email.create_message();
+        let mailer = Mailer::new(mailer_configuration);
+        match mailer.send_message(message) {
+            Ok(_) => info!("Mail sent successfully!"),
+            Err(e) => error!("Mailer failed: {:?}", e),
+        }
+    }
+    Ok(Json(email))
+}
