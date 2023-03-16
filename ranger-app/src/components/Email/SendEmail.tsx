3d84907a4de72b4f22818d3a85861ff7c5d4dcb1
Kristi Reispass <kristi.reispass@cr14.ee>
2023-03-16 09:53:19 +0000

    remove unnecessary error handling

--- ranger-app/src/components/Email/SendEmail.tsx
+++ ranger-app/src/components/Email/SendEmail.tsx
@@ -96,85 +96,67 @@ const SendEmail = ({exercise}: {exercise: Exercise}) => {
           control={control}
           name='replyToAddress'
           render={({
-            field: {onChange, onBlur, ref, value}, fieldState: {error},
-          }) => {
-            const intent = error ? Intent.DANGER : Intent.NONE;
-            return (
-              <FormGroup
-                labelFor='email-reply-to'
-                helperText={error?.message}
-                intent={intent}
-                label={t('emails.form.replyTo.title')}
-              >
-                <InputGroup
-                  large
-                  type='email'
-                  intent={intent}
-                  value={value}
-                  inputRef={ref}
-                  id='email-reply-to'
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-              </FormGroup>
-            );
-          }}
+            field: {onChange, onBlur, ref, value},
+          }) => (
+            <FormGroup
+              labelFor='email-reply-to'
+              label={t('emails.form.replyTo.title')}
+            >
+              <InputGroup
+                large
+                type='email'
+                value={value}
+                inputRef={ref}
+                id='email-reply-to'
+                onChange={onChange}
+                onBlur={onBlur}
+              />
+            </FormGroup>
+          )}
         />
         <Controller
           control={control}
           name='ccAddress'
           render={({
-            field: {onChange, onBlur, ref, value}, fieldState: {error},
-          }) => {
-            const intent = error ? Intent.DANGER : Intent.NONE;
-            return (
-              <FormGroup
-                labelFor='email-cc'
-                helperText={error?.message}
-                intent={intent}
-                label={t('emails.form.cc.title')}
-              >
-                <InputGroup
-                  large
-                  type='email'
-                  intent={intent}
-                  value={value}
-                  inputRef={ref}
-                  id='email-cc'
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-              </FormGroup>
-            );
-          }}
+            field: {onChange, onBlur, ref, value},
+          }) => (
+            <FormGroup
+              labelFor='email-cc'
+              label={t('emails.form.cc.title')}
+            >
+              <InputGroup
+                large
+                type='email'
+                value={value}
+                inputRef={ref}
+                id='email-cc'
+                onChange={onChange}
+                onBlur={onBlur}
+              />
+            </FormGroup>
+          )}
         />
         <Controller
           control={control}
           name='bccAddress'
           render={({
-            field: {onChange, onBlur, ref, value}, fieldState: {error},
-          }) => {
-            const intent = error ? Intent.DANGER : Intent.NONE;
-            return (
-              <FormGroup
-                labelFor='email-bcc'
-                helperText={error?.message}
-                intent={intent}
-                label={t('emails.form.bcc.title')}
-              >
-                <InputGroup
-                  large
-                  type='email'
-                  intent={intent}
-                  value={value}
-                  inputRef={ref}
-                  id='email-bcc'
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-              </FormGroup>
-            );
-          }}
+            field: {onChange, onBlur, ref, value},
+          }) => (
+            <FormGroup
+              labelFor='email-bcc'
+              label={t('emails.form.bcc.title')}
+            >
+              <InputGroup
+                large
+                type='email'
+                value={value}
+                inputRef={ref}
+                id='email-bcc'
+                onChange={onChange}
+                onBlur={onBlur}
+              />
+            </FormGroup>
+          )}
         />
         <Controller
           control={control}
