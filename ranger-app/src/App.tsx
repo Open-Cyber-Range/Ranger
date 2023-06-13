3cd53ea18e142e27d11053dc49d2d25237c262c8
Kristi Reispass <kristi.reispass@cr14.ee>
2023-06-13 07:15:54 +0000

    add participant navbar

--- ranger-app/src/App.tsx
+++ ranger-app/src/App.tsx
@@ -5,27 +5,52 @@ import Exercises from 'src/pages/Exercises';
 import MainNavbar from 'src/components/MainNavBar';
 import Home from 'src/pages/Home';
 import {useKeycloak} from '@react-keycloak/web';
+import {useSelector} from 'react-redux';
+import ParticipantNavBar from './components/ParticipantNavBar';
 import DeploymentDetail from './pages/DeploymentDetail';
 import EmailLog from './pages/EmailLog';
 import SendEmail from './pages/Email';
+import {selectedRoleSelector} from './slices/userSlice';
+import {UserRole} from './models/userRoles';
 
 const App = () => {
   const {keycloak: {authenticated}} = useKeycloak();
-  return authenticated ? (
-    <Router>
-      <MainNavbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/exercises' element={<Exercises/>}/>
-        <Route path='/exercises/:exerciseId' element={<ExerciseDetail/>}/>
-        <Route path='/exercises/:exerciseId/email' element={<SendEmail/>}/>
-        <Route
-          path='/exercises/:exerciseId/deployments/:deploymentId'
-          element={<DeploymentDetail/>}/>
-        <Route path='/exercises/:exerciseId/emails' element={<EmailLog/>}/>
-      </Routes>
-    </Router>
-  ) : null;
+  const currentRole = useSelector(selectedRoleSelector);
+  if (authenticated && (currentRole === UserRole.MANAGER)) {
+    return (
+      <Router>
+        <MainNavbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/exercises' element={<Exercises/>}/>
+          <Route path='/exercises/:exerciseId' element={<ExerciseDetail/>}/>
+          <Route path='/exercises/:exerciseId/email' element={<SendEmail/>}/>
+          <Route
+            path='/exercises/:exerciseId/deployments/:deploymentId'
+            element={<DeploymentDetail/>}/>
+          <Route path='/exercises/:exerciseId/emails' element={<EmailLog/>}/>
+        </Routes>
+      </Router>
+    );
+  }
+
+  if (authenticated && (currentRole === UserRole.PARTICIPANT)) {
+    return (
+      <Router>
+        <ParticipantNavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/exercises' element={<Exercises/>}/>
+          <Route path='/exercises/:exerciseId' element={<ExerciseDetail/>}/>
+          <Route
+            path='/exercises/:exerciseId/deployments/:deploymentId'
+            element={<DeploymentDetail/>}/>
+        </Routes>
+      </Router>
+    );
+  }
+
+  return null;
 };
 
 export default App;
