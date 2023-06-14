c388874ea0c7b0d84108e356a323e4ebe1f5a18e
Kaarel Allemann <kaarel.allemann@cr14.ee>
2023-06-14 13:51:58 +0000

    add getting exercise

--- ranger/src/main.rs
+++ ranger/src/main.rs
@@ -91,7 +91,11 @@ async fn main() -> Result<(), Error> {
                             .service(get_exercise_deployment_scenario)
                             .service(get_exercise_deployment_users)
                             .service(get_exercise_deployment_scores)
-                            .service(scope("/exercise").service(get_participant_exercises))
+                            .service(
+                                scope("/exercise")
+                                    .service(get_participant_exercises)
+                                    .service(get_exercise),
+                            )
                             .wrap(participant_auth_middleware),
                     ),
             )
