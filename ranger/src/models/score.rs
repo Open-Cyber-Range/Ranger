21edc312951749e482a8504b50eb5181f2026dca
Robert Altmäe <robert.altmae@cr14.ee>
2023-06-15 13:34:43 +0300

    Add correct metric name to scoring websocket message

--- ranger/src/models/score.rs
+++ ranger/src/models/score.rs
@@ -1,8 +1,9 @@
 use super::helpers::uuid::Uuid;
+use crate::models::ConditionMessage;
 use bigdecimal::BigDecimal;
 use chrono::NaiveDateTime;
+use sdl_parser::metric::Metrics;
 use serde::{Deserialize, Serialize};
-use crate::models::ConditionMessage;
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -39,18 +40,36 @@ impl Score {
             timestamp,
         }
     }
-}
 
-impl From<ConditionMessage> for Score {
-    fn from(condition_message: ConditionMessage) -> Self {
-        Score {
+    pub fn from_conditionmessage_and_metrics(
+        condition_message: ConditionMessage,
+        metrics: Option<Metrics>,
+    ) -> Self {
+        let metric_name = if let Some(metrics) = metrics {
+            if let Some((metric_key, metric)) = metrics.iter().find(|(_, metric)| {
+                metric
+                    .condition
+                    .eq(&Some(condition_message.clone().condition_name))
+            }) {
+                match &metric.name {
+                    Some(metric_name) => metric_name.to_owned(),
+                    None => metric_key.to_owned(),
+                }
+            } else {
+                condition_message.condition_name
+            }
+        } else {
+            condition_message.condition_name
+        };
+
+        Self {
             id: condition_message.id,
             exercise_id: condition_message.exercise_id,
             deployment_id: condition_message.deployment_id,
-            metric_name: condition_message.condition_name,
+            metric_name,
             vm_name: condition_message.virtual_machine_id.to_string(),
             vm_uuid: condition_message.virtual_machine_id,
-            value: condition_message.value,
+            value: condition_message.value.clone(),
             timestamp: condition_message.created_at,
         }
     }
