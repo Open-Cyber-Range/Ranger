97c0d6514c5a7150b9b1ccc8ddbf214851e1e6dc
Kristi Reispass <kristi.reispass@cr14.ee>
2023-07-12 07:25:23 +0000

    update participant deployment view

--- ranger-app/src/pages/participant/DeploymentDetail.tsx
+++ ranger-app/src/pages/participant/DeploymentDetail.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import type {DeploymentDetailRouteParameters} from 'src/models/routes';
 import {useTranslation} from 'react-i18next';
 import {skipToken} from '@reduxjs/toolkit/dist/query';
-import {useParticipantGetDeploymentQuery} from 'src/slices/apiSlice';
-import SideBar from 'src/components/Exercise/SideBar';
-import ParticipantDeploymnentGraph from 'src/components/Scoring/participant/Graph';
+import {
+  useParticipantGetDeploymentQuery,
+  useParticipantGetDeploymentScenarioQuery,
+} from 'src/slices/apiSlice';
+import SideBar from 'src/components/Exercise/participant/SideBar';
+import ParticipantDeploymentGraph from 'src/components/Scoring/participant/Graph';
 import {H2} from '@blueprintjs/core';
+import EntitySelect from 'src/components/EntitySelect';
+import {type Entity} from 'src/models/scenario';
+import TloTable from 'src/components/Scoring/participant/TloTable';
 
 const ParticipantDeploymentDetail = () => {
   const {t} = useTranslation();
@@ -14,16 +20,42 @@ const ParticipantDeploymentDetail = () => {
   const {data: deployment} = useParticipantGetDeploymentQuery(
     exerciseId && deploymentId ? {exerciseId, deploymentId} : skipToken,
   );
+  const {data: scenario} = useParticipantGetDeploymentScenarioQuery(exerciseId && deploymentId
+    ? {exerciseId, deploymentId} : skipToken);
+
+  const [selectedEntityKey, setSelectedEntityKey] = useState<string | undefined>(undefined);
+
+  const handleEntityChange = (selectedKey: string | undefined) => {
+    setSelectedEntityKey(selectedKey);
+  };
+
+  let selectedEntity: Entity | undefined;
+  if (scenario?.entities) {
+    selectedEntity = scenario?.entities[selectedEntityKey ?? ''];
+  }
 
   if (exerciseId && deploymentId) {
     return (
       <SideBar renderMainContent={() => (
         <>
           <H2>{deployment?.name}</H2>
+
+          <EntitySelect
+            entities={scenario?.entities}
+            selectedEntityKey={selectedEntityKey}
+            onChange={handleEntityChange}
+          />
           <br/>
-          <ParticipantDeploymnentGraph
+          <ParticipantDeploymentGraph
             exerciseId={exerciseId}
             deploymentId={deploymentId}
+          />
+
+          <TloTable
+            exerciseId={exerciseId}
+            deploymentId={deploymentId}
+            tloMap={scenario?.tlos}
+            selectedEntity={selectedEntity}
           />
         </>
       )}
