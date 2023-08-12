1e3e9d599479f87d5fa2f29ec9a7758c627b362d
Rait Rotšan <rait.rotsan@cr14.ee>
2023-08-12 15:01:34 +0000

    add metric endpoints to apiSlice

--- ranger-app/src/models/manualMetric.ts
+++ ranger-app/src/models/manualMetric.ts
@@ -1,13 +1,29 @@
 import {type ExerciseRole} from './scenario';
 
-export type DummyManualMetric = {
-  id: string;
-  name: string;
-  maxScore: number;
+export type NewManualMetric = {
+  exerciseId: string;
+  deploymentId: string;
+  entitySelector: string;
+  metricKey: string;
   role: ExerciseRole;
-  textSubmissionValue?: string;
+  textSubmission?: string;
 };
 
-export type UpdateDummyManualMetric = {
-  score: number;
-} & DummyManualMetric;
+export type ManualMetric = {
+  id: string;
+  userId: string;
+  name: string;
+  description: string;
+  score?: number;
+  maxScore: number;
+} & NewManualMetric;
+
+export type UpdateManualMetric = {
+  textSubmission?: string;
+  score?: number;
+};
+
+export type FetchArtifact = {
+  filename?: string;
+  url?: string;
+};
