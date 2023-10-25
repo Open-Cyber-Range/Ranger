0b634be5ffdae674f946ab59802f1827ca8c8277
Robert Altmäe <robert.altmae@cr14.ee>
2023-10-25 15:57:25 +0300

    Headings for every subpage

--- ranger-app/src/pages/ExerciseDetail.tsx
+++ ranger-app/src/pages/ExerciseDetail.tsx
@@ -12,8 +12,11 @@ import SendEmail from 'src/components/Email/SendEmail';
 import SideBar from 'src/components/Exercise/SideBar';
 import useExerciseStreaming from 'src/hooks/useExerciseStreaming';
 import {ActiveTab} from 'src/models/exercise';
+import {H2} from '@blueprintjs/core';
+import {useTranslation} from 'react-i18next';
 
 const ExerciseDetail = () => {
+  const {t} = useTranslation();
   const {exerciseId} = useParams<ExerciseDetailRouteParameters>();
   useExerciseStreaming(exerciseId);
   const {data: deployments} = useAdminGetDeploymentsQuery(exerciseId ?? skipToken);
@@ -23,14 +26,27 @@ const ExerciseDetail = () => {
     return (
       <SideBar renderMainContent={activeTab => (
         <>
-          {activeTab === ActiveTab.Dash && (<DashboardPanel
-            exercise={exercise}
-            deployments={deployments}
-          />)}
-          {activeTab === ActiveTab.Scores && (<ScoresPanel
-            deployments={deployments}
-          />)}
-          {activeTab === ActiveTab.Emails && (<SendEmail exercise={exercise}/>)}
+          {activeTab === ActiveTab.Dash && (
+            <>
+              <H2>{t('exercises.tabs.dashboard')}</H2>
+              <DashboardPanel
+                exercise={exercise}
+                deployments={deployments}
+              />
+            </>
+          )}
+          {activeTab === ActiveTab.Scores && (
+            <>
+              <H2>{t('exercises.tabs.scores')}</H2>
+              <ScoresPanel
+                deployments={deployments}
+              />
+            </>)}
+          {activeTab === ActiveTab.Emails && (
+            <>
+              <H2>{t('exercises.tabs.emails')}</H2>
+              <SendEmail exercise={exercise}/>
+            </>)}
         </>
       )}/>
     );
