aef4618c9dcf58c4a2bdc2c0bb7c3b1ea1078b3e
Rait Rotšan <rait.rotsan@cr14.ee>
2023-12-05 12:08:18 +0000

    update home page

--- ranger-app/src/pages/Home.tsx
+++ ranger-app/src/pages/Home.tsx
@@ -1,14 +1,50 @@
-import {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import backgroundImage from 'src/assets/cr14_taust.jpg';
 
 const Home = () => {
-  const navigate = useNavigate();
+  const {t} = useTranslation();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    navigate('/exercises');
-  }, [navigate]);
+    const img = new Image();
+    img.src = backgroundImage;
+    img.addEventListener('load', () => {
+      setLoading(false);
+    });
+  }, []);
 
-  return null;
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <div
+          className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500'/>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className='flex flex-col h-full justify-center items-center
+      bg-home-background bg-cover bg-no-repeat bg-center'
+    >
+      {!loading && (
+        <div
+          className='text-center font-bold text-white bg-black bg-opacity-50 p-8 rounded-3xl'
+        >
+          <h1 className='mb-4 text-7xl tracking-wider uppercase'>{t('appName')}</h1>
+          <p className='mb-8 py-4 text-2xl border-y'>{t('OCR')}</p>
+          <a
+            href='https://documentation.opencyberrange.ee/docs/category/ranger'
+            className='inline-block px-8 py-2 text-base text-center text-white no-underline
+          bg-transparent border-2 border-white rounded-full hover:bg-white hover:text-black'
+          >
+            {t('documentation')}
+          </a>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
