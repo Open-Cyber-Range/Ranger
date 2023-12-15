c0021d30200526cb704973fa5c6b5e303ed8f00a
Rait Rotšan <rait.rotsan@cr14.ee>
2023-12-15 14:17:17 +0000

    fix home background image path

--- ranger-app/tailwind.config.js
+++ ranger-app/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'home-background': "url('src/assets/cr14_taust.jpg')",
+        'home-background': "url('/src/assets/cr14_taust.jpg')",
       }),
       boxShadow: {
         'inner': 'inset 0 0 0 1px rgba(17, 20, 24, 0.2),  inset 0 1px 1px rgba(17, 20, 24, 0.5)'
