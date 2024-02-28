90808cc0e219954b2bccaca4fd2efcbf14f740d6
Madis-Mikk Remmet <madismikk.remmet@cr14.ee>
2024-02-28 08:48:13 +0000

    add loading spinner while exercises are being fetched

--- ranger-app/src/components/Exercise/participant/List.tsx
+++ ranger-app/src/components/Exercise/participant/List.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {useParticipantGetExercisesQuery} from 'src/slices/apiSlice';
 import humanInterval from 'human-interval';
 import {sortByProperty} from 'sort-by-property';
+import {Spinner} from '@blueprintjs/core';
 import ExerciseDeployments from './ExerciseDeployments';
 
 const ExerciseList = () => {
   const {
-    data: potentialExercises,
+    data: potentialExercises, isLoading,
   } = useParticipantGetExercisesQuery(
     undefined,
     {pollingInterval: humanInterval('5 seconds')},
@@ -14,13 +15,21 @@ const ExerciseList = () => {
   let exercises = potentialExercises ?? [];
   exercises = exercises.slice().sort(sortByProperty('updatedAt', 'desc'));
 
+  if (!isLoading) {
+    return (
+      <div className='flex flex-col items-center justify-center space-y-4 min-h-screen'>
+        <h4>Loading exercises, please wait.</h4>
+        <Spinner/>
+      </div>
+    );
+  }
+
   return (
     <div className='flex flex-col gap-8'>
       {exercises.map(exercise => (
         <ExerciseDeployments key={exercise.id} exercise={exercise}/>
       ))}
     </div>
-
   );
 };
 
